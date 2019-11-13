5141581edb67879eddd6877c90c4e6cbdf73f6b6
Nory Lazo <nlazo@icloud.com>
2019-11-13 03:47:30 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,81 @@
+def sort_array_asc(integers)
+  integers.sort
+end
+
+def sort_array_desc(integers)
+  integers.sort do |a, b|
+    b <=> a
+  end 
+end 
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length 
+  end 
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(integers)
+  integers.reverse
+end
+
+def kesha_maker(strings)
+  strings.each do |word|
+    word[2] = "$"
+  end
+end
+
+def find_a(strings)
+  array = []
+  # array << if strings.start_with?("a")
+  
+  i = 0 
+  while i < strings.length
+    if strings[i].start_with?("a")
+      array << strings[i]
+    end
+    i += 1
+  end
+  array
+end
+
+def sum_array(integers)
+  integers.inject(0){ |sum, num| sum + num } 
+  
+  # sum = 0
+  # integers.each do |num|
+  #   sum+=num
+  # end
+  # sum
+end 
+
+def add_s(array)
+  # array.each_with_index { |value, index |
+    
+  #   if value != array[1]
+  #     array.collect { |word| word + "s" }
+  #   end
+  
+  # array.each do |word|
+    
+  #   if array[1] == word
+  #     word
+  #   else
+  #     array.collect { |word| word + "s" }
+  #   end
+  # end
+  # array
+  
+  array.collect do |word|
+    if array[1] == word
+      word 
+    else 
+      word + "s"
+    end
+  end
+end
 
