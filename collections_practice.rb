3af46f974cab18eb4618c7c9472be96bd1a1057f
Nory Lazo <nlazo@icloud.com>
2019-11-13 03:26:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -44,7 +44,7 @@ def find_a(strings)
 end
 
 def sum_array(integers)
-  integers.inject{ |sum, num| sum + num } 
+  integers.inject(1){ |sum, num| sum + num } 
   
   # sum = 0
   # integers.each do |num|
