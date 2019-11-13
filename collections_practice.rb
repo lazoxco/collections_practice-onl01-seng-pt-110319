45e2f7745f4a984dbb7ce7a62306cc0f107d844a
Nory Lazo <nlazo@icloud.com>
2019-11-13 03:33:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -54,6 +54,6 @@ def sum_array(integers)
 end 
 
 def add_s(array)
-  array.each_with_index.collect { |element, index| if element == array[index]}
+  array.each_with_index.collect { |element, index| if element == array[1]}
 end
 
